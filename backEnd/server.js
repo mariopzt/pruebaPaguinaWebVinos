4bd9c10524c791d978bcb593cda2ea086f58880d
Mario <mariopereztoranzo1@gmail.com>
2025-12-22 23:01:39 +0100

    nuevo vino db

--- backEnd/server.js
+++ backEnd/server.js
@@ -13,9 +13,9 @@ connectDB();
 // Crear app de Express
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Middleware (aumentamos límite para imágenes base64 pequeñas)
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // CORS
 app.use(cors({
